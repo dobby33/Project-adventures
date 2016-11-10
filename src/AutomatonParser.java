4ae88ff4f96bb45e3e0b1c108bf715c494dfa4e0
Bas Van Assche <basvanassche@gmail.com>
2016-11-10 12:25:42 +0100

    merge conflict

--- src/AutomatonParser.java
+++ src/AutomatonParser.java
@@ -1,7 +1,23 @@
 /* Dit project is gemaakt door Bas Van Assche en Tom Martens*/
 public class AutomatonParser {
+    dit is
+    publ
+    een merge
+    ic Automaton
+
     public AutomatonParser(String filename){
         /*De constructor leest een .aut bestand filename in.*/
+
+    }
+
+    intersection(Automaton aut)
+    {
+        /*Deze methode geeft een eindige automaat terug waarvan de taal gelijk is aan de intersectie
+        van de taal L1 van de automaat van de klasse en taal L2 van de automaat aut.
+        Hint: gebruik de productconstructie. Merk echter op dat de productconstructie in het boek
+        van Sipser zich beperkt tot DFA’s. Bekijk zelf hoe je het idee van de productconstructie kunt
+        toepassen op NFA’s. (Een alternatieve methode van NFA’s eerst omzetten naar DFA’s en dan
+        de productconstructie van Sipser uitvoeren is waarschijnlijk te traag voor de voorbeeldadventures.)*/
 
     }
 
@@ -17,15 +33,7 @@ public class AutomatonParser {
 
     }
 
-    public Automaton intersection(Automaton aut){
-        /*Deze methode geeft een eindige automaat terug waarvan de taal gelijk is aan de intersectie
-        van de taal L1 van de automaat van de klasse en taal L2 van de automaat aut.
-        Hint: gebruik de productconstructie. Merk echter op dat de productconstructie in het boek
-        van Sipser zich beperkt tot DFA’s. Bekijk zelf hoe je het idee van de productconstructie kunt
-        toepassen op NFA’s. (Een alternatieve methode van NFA’s eerst omzetten naar DFA’s en dan
-        de productconstructie van Sipser uitvoeren is waarschijnlijk te traag voor de voorbeeldadventures.)*/
-
-    }
+    conflict
 
     public String getShortestExample(Boolean accept){
         /*Deze methode schrijft een kortste string uit die door de automaat wel (niet, respectievelijk)*/
