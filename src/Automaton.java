5088c80fe4f486373dc22a0b4a31d4017cd61245
Bas Van Assche <basvanassche@gmail.com>
2016-11-25 12:03:32 +0100

    - Commentaar bijgevoegd - Automatom getest - Bug fixed: oneindige recursie op lege string

--- src/Automaton.java
+++ src/Automaton.java
@@ -7,11 +7,24 @@ import java.util.List;
  */
 public class Automaton
 {
+    /**
+     * De lijst met alle overgangen tussen twee states
+     */
     private List<Edge> _edges;
+    /**
+     * De start state
+     */
     private String _start;
-    private List<String> _finish;
+    /**
+     * Een verzameling met eind states
+     */
+    private HashSet<String> _finish;
+    /**
+     * Een verzameling van alle states
+     */
     private HashSet<String> _states;
 
+    /* Dit zijn variabele voor het zoeken van het korste pad */
     private String _minCombinatie;
     private int _minNEdges;
     private int _result;
@@ -19,7 +32,7 @@ public class Automaton
     public Automaton()
     {
         _edges = new ArrayList<>();
-        _finish = new ArrayList<>();
+        _finish = new HashSet<>();
         _states = new HashSet<>();
     }
 
@@ -33,6 +46,7 @@ public class Automaton
     public Automaton intersection(Automaton aut)
     {
         Automaton result = new Automaton();
+        // de start state zetten
         result._start = concatStates(_start, aut._start);
         // De finish states combineren
         for (String state1 : _finish)
@@ -43,7 +57,9 @@ public class Automaton
         for (Edge edge1 : _edges)
             for (Edge edge2 : aut._edges)
                 if (edge1.getWeigth().equals(edge2.getWeigth()))
-                    result.addEdge(concatStates(edge1.getFrom(), edge2.getFrom()), concatStates(edge1.getTo(), edge2.getTo()), edge1.getWeigth());
+                    result.addEdge(concatStates(edge1.getFrom(), edge2.getFrom()),
+                                   concatStates(edge1.getTo(), edge2.getTo()),
+                                   edge1.getWeigth());
         return result;
     }
 
@@ -52,20 +68,65 @@ public class Automaton
      *
      * @param accept : true: korste string die wordt geaccepteerd
      *               false: de korste string die niet wordt geaccepteerd
-     * @return De gevonden string
-     * @custom.pre : De automaat is correct opgebouwd
-     * @custom.post : string bevat geen $
+     * @return De korst string, of null indien deze niet bestaat.
+     * @pre : De automaat is correct opgebouwd
+     * @post : string bevat geen $
      */
     public String getShortestExample(Boolean accept)
     {
-        if (accept)
-        {
+        if (accept) {
             shortestAccept();
             return _minCombinatie;
-        } else
-        {
-            return null;
+        } else {
+            shortestNotAccept();
+            return _minCombinatie;
         }
+    }
+
+    /**
+     * Deze functie moet opgeroepen worden wanneer men het kortste pad wil vinden in de automaat
+     * dat niet wordt geaccepteerd.
+     *
+     * Het resultaat wordt opgeslagen in {@link Automaton#_minCombinatie}
+     * Het aantal adges van dit resulaat staat in {@link Automaton#_minNEdges}
+     */
+    private void shortestNotAccept()
+    {
+        _minNEdges = getNumberOfStates();   // We willen geen pad dat langer is dan het aantal states
+        _minCombinatie = null;  // Als er geen korste pad wordt gevonden wordt er null terug gegeven
+        shortestNotAccept(_start, "", 0);
+    }
+
+    /**
+     * Deze functie zoekt door middle van recursie het kortste pad in de automaat dat niet wordt geaccepteerd. De eerste keer moet men deze functie aanroepen zonder paramters
+     * @param state : de huidige state
+     * @param combinatie : Het pad dat we tot nu toe hebben
+     * @param nEdges : het aantal egdes dat we reeds zij gepasseerd
+     *
+     * Het resultaat wordt opgeslagen in {@link Automaton#_minCombinatie}
+     * Het aantal adges van dit resulaat staat in {@link Automaton#_minNEdges}
+     */
+    private void shortestNotAccept(String state, String combinatie, int nEdges)
+    {
+        if (nEdges >= _minNEdges) // Als we al een korter pad hebben gevonden
+            return;
+        if (!_finish.contains(state))  // Als er een kleinere combinatie is gevonden
+        {
+            // Deze string wordt niet geaccepteerd
+            _minNEdges = nEdges;
+            _minCombinatie = combinatie;
+            return;
+        }
+
+        // Als het wel een accept state is
+        // We gaan alle nieuwe edges af
+        for (Edge edge : _edges) {
+            if (edge.getFrom().equals(state) && "$".equals(edge.getWeigth()))   // als het een lege string is
+                shortestNotAccept(edge.getTo(), combinatie, nEdges + 1);
+            else if (edge.getFrom().equals(state))
+                shortestNotAccept(edge.getTo(), combinatie + edge.getWeigth(), nEdges + 1);
+        }
+
     }
 
     /**
@@ -81,29 +142,37 @@ public class Automaton
      */
     private void shortestAccept()
     {
-        _minNEdges = getNumberOfStates();
-        _minCombinatie = null;
+        _minNEdges = getNumberOfStates();  // We zeggen dat het te zoeken pad kleiner moet zijn dan het aantal states
+        _minCombinatie = null;  // Als er geen combinatie wordt gevonden zal er dus null in het resultaat staan
         shortestAccept(_start, "", 0);
     }
 
     /**
-     * Hierop wordt de recurtie gedaan, eerste keer functie zonder parameters aanroepen
+     * Hierop wordt de recursie gedaan, eerste keer functie zonder parameters aanroepen
+     *
+     * @param state      : Het label van de huidige state
+     * @param combinatie : De labels van de edges achterelkaar die we tot nu toe hebben
+     * @param nEdges : De hoeveelheid edges die we reeds hebben gedaan.
+     *
+     * Het resultaat wordt opgeslagen in {@link Automaton#_minCombinatie}
+     * Het aantal adges van dit resulaat staat in {@link Automaton#_minNEdges}
      */
     private void shortestAccept(String state, String combinatie, int nEdges)
     {
-        if (nEdges >= _minNEdges)
+        if (nEdges >= _minNEdges)  // Als we al een combinatie hebben die korter is dan deze combinatie
             return;
-        if (_finish.contains(state))  // en nEdges is kleiner den _minEdges
+        if (_finish.contains(state)) // Als er een kleinere combinatie is gevonden
         { // State is een accept state
             _minCombinatie = combinatie;
             _minNEdges = nEdges;
             return;
         }
 
-        for (Edge edge : _edges)
-        {
-            if (edge.getFrom().equals(state) && "$".equals(edge.getWeigth()))
-                shortestAccept(edge.getTo(), combinatie + edge.getWeigth(), nEdges);
+        // 'state' is nog geen accept state
+        // We moeten dus nog edges toevoegen
+        for (Edge edge : _edges) {
+            if (edge.getFrom().equals(state) && "$".equals(edge.getWeigth()))   // als het een lege string is
+                shortestAccept(edge.getTo(), combinatie, nEdges + 1);
             else if (edge.getFrom().equals(state))
                 shortestAccept(edge.getTo(), combinatie + edge.getWeigth(), nEdges + 1);
         }
@@ -125,7 +194,7 @@ public class Automaton
         _states.add(start);
     }
 
-    public List<String> getFinish()
+    public HashSet<String> getFinish()
     {
         return _finish;
     }
@@ -136,7 +205,7 @@ public class Automaton
     }
 
     /**
-     * Voegt een nieuwe state toe aan de automaat
+     * Voegt een nieuwe overgang toe aan de automaat.
      *
      * @param from   : De String state vanwaar de boog vertrekt
      * @param to     : De string van de aankomst state
@@ -145,10 +214,15 @@ public class Automaton
     public void addEdge(String from, String to, String weight)
     {
         _edges.add(new Edge(from, to, weight));
-        _states.add(from);
-        _states.add(to);
+        _states.add(from);      // gaat 'from' enkel toevoegen als 'from' er nog niet in zit
+        _states.add(to);        // hetzelfde voor 'to'
     }
 
+    /**
+     * Deze functie voegt een nieuwe eindstate toe.
+     *
+     * @param finish : de naam van de eindtoestand.
+     */
     public void addFinish(String finish)
     {
         _finish.add(finish);
@@ -169,24 +243,32 @@ public class Automaton
 
     private int getNumberOfStates()
     {
-        int _result = 0;
-        for (Edge edge : _edges)
-            _result++;
-        return _result;
+        return _states.size();
     }
 
 
     @Override
     public String toString()
     {
-        String result = "{\n";
-        result += "Start: " + _start;
-        result += "\nFinish: " + _finish;
-        result += "\nEdges: ";
-        for (Edge edge : _edges)
-            result += "\n\t" + edge;
-        result += "\n}";
-        return result;
+        StringBuilder result = new StringBuilder();
+        result.append("Automaat:\n");
+        /* start */
+        result.append("\t").append("Start:\n");
+        result.append("\t|\t").append(_start).append("\n");
+        /* edges */
+        result.append("\t").append("Edges:\n");
+        for (Edge edge: _edges)
+            result.append("\t|\t").append(edge).append("\n");
+        /* states */
+        result.append("\t").append("States:\n");
+        for (String state: _states)
+            result.append("\t|\t").append(state).append("\n");
+        /* finish states */
+        result.append("\t").append("Finish states:\n");
+        for (String state: _finish)
+            result.append("\t|\t").append(state).append("\n");
+
+        return result.toString();
     }
 }
 
--- src/TestingMain.java
+++ src/TestingMain.java
@@ -5,99 +5,130 @@ public class TestingMain
 {
     public static void main(String[] agrs)
     {
-        // autommaat voor het vinden van een sleutel voor je door een deur gaat
-        Automaton aut2 = new Automaton();
-        aut2.setStart("1");
-        aut2.addFinish("1");
-        aut2.addFinish("2");
-        aut2.addEdge("1", "1", "D"); // <--
-        aut2.addEdge("1", "1", "S"); // <--
-        aut2.addEdge("1", "1", "A"); // <--
-        aut2.addEdge("1", "1", "R"); // <--
-        aut2.addEdge("1", "2", "K"); // KEY --> 2
-        aut2.addEdge("1", "3", "G"); // GATE --> 3
-        aut2.addEdge("1", "1", "T"); // <--
+        // autommaat voor minstens twee schatten te vinden
 
-        aut2.addEdge("2", "2", "D"); // <--
-        aut2.addEdge("2", "2", "S"); // <--
-        aut2.addEdge("2", "2", "A"); // <--
-        aut2.addEdge("2", "2", "R"); // <--
-        aut2.addEdge("2", "2", "K"); // <--
-        aut2.addEdge("2", "2", "G"); // <--
-        aut2.addEdge("2", "2", "T"); // <--
-
-        aut2.addEdge("3", "3", "D"); // <--
-        aut2.addEdge("3", "3", "S"); // <--
-        aut2.addEdge("3", "3", "A"); // <--
-        aut2.addEdge("3", "3", "R"); // <--
-        aut2.addEdge("3", "3", "K"); // <--
-        aut2.addEdge("3", "3", "G"); // <--
-        aut2.addEdge("3", "3", "T"); // <--
-
-        // automaat om na het tegenkomen van een draak in de rivier te springen als je geen zwaard hebt
-        Automaton aut3 = new Automaton();
-        aut3.setStart("1");
-        aut3.addFinish("1");
-        aut3.addFinish("2");
-        aut3.addFinish("4");
-        aut3.addEdge("1", "3", "D"); // DRAGON --> 3
-        aut3.addEdge("1", "2", "S"); // SWORD --> 2
-        aut3.addEdge("1", "1", "A"); // <--
-        aut3.addEdge("1", "1", "R"); // <--
-        aut3.addEdge("1", "1", "K"); // <--
-        aut3.addEdge("1", "1", "G"); // <--
-        aut3.addEdge("1", "1", "T"); // <--
-
-        aut3.addEdge("2", "2", "D"); // <--
-        aut3.addEdge("2", "2", "S"); // <--
-        aut3.addEdge("2", "2", "A"); // <--
-        aut3.addEdge("2", "2", "R"); // <--
-        aut3.addEdge("2", "2", "K"); // <--
-        aut3.addEdge("2", "2", "G"); // <--
-        aut3.addEdge("2", "2", "T"); // <--
-
-        aut3.addEdge("3", "4", "R"); // RIVER --> 4
-
-        aut3.addEdge("4", "3", "D"); // DRAGON --> 3
-        aut3.addEdge("4", "2", "S"); // SWORD --> 2
-        aut3.addEdge("4", "1", "A"); // --> 1
-        aut3.addEdge("4", "1", "R"); // --> 1
-        aut3.addEdge("4", "1", "K"); // --> 1
-        aut3.addEdge("4", "1", "G"); // --> 1
-        aut3.addEdge("4", "1", "T"); // --> 1
-
-        // autommaat om minstens twee schatten te vinden na het passeren van de laatste boog
-        Automaton aut4 = new Automaton();
-        aut4.setStart("1");
-        aut4.addFinish("3");
-        aut4.addEdge("1", "1", "D"); // <--
-        aut4.addEdge("1", "1", "S"); // <--
-        aut4.addEdge("1", "1", "A"); // <--
-        aut4.addEdge("1", "1", "R"); // <--
-        aut4.addEdge("1", "1", "K"); // <--
-        aut4.addEdge("1", "1", "G"); // <--
-        aut4.addEdge("1", "2", "T"); // TREASURE --> 2
-
-        aut4.addEdge("2", "2", "D"); // <--
-        aut4.addEdge("2", "2", "S"); // <--
-        aut4.addEdge("2", "1", "A"); // ARK --> 1
-        aut4.addEdge("2", "2", "R"); // <--
-        aut4.addEdge("2", "2", "K"); // <--
-        aut4.addEdge("2", "2", "G"); // <--
-        aut4.addEdge("2", "3", "T"); // TREASURE --> 3
-
-        aut4.addEdge("3", "3", "D"); // <--
-        aut4.addEdge("3", "3", "S"); // <--
-        aut4.addEdge("3", "1", "A"); // ARK --> 1
-        aut4.addEdge("3", "3", "R"); // <--
-        aut4.addEdge("3", "3", "K"); // <--
-        aut4.addEdge("3", "3", "G"); // <--
-        aut4.addEdge("3", "3", "T"); // <--
-        Automaton result = aut2.intersection(aut3).intersection(aut4);
-
+        Automaton result = LoadInTest5();
+        System.out.println("Korst geaccepteerd: " + result.getShortestExample(true));
+        System.out.println("Korst niet geaccepteerd: " + result.getShortestExample(false));
 
         AutomatonViewer view = new AutomatonViewer(result);
-        view.PrintInAutFormat();
-        view.SavePs("..\\Aut2.ps");
+//        view.PrintInAutFormat();
+//        view.SavePs("..\\Intersection.ps");
+    }
+
+    static private Automaton LoadInTest1()
+    {
+        Automaton aut1 = new Automaton();
+        Automaton aut2 = new Automaton();
+        // Test is gelukt
+        aut1.addEdge("1", "2", "a");
+        aut1.addEdge("2", "3", "a");
+        aut1.addEdge("3", "4", "a");
+        aut1.addEdge("1", "1", "b");
+        aut1.addEdge("2", "2", "b");
+        aut1.addEdge("3", "3", "b");
+        aut1.addEdge("4", "4", "b");
+        aut1.addEdge("4", "4", "a");
+        aut1.setStart("1");
+        aut1.addFinish("3");
+
+
+        aut2.addEdge("1", "2", "b");
+        aut2.addEdge("2", "3", "b");
+        aut2.addEdge("1", "1", "a");
+        aut2.addEdge("2", "2", "a");
+        aut2.addEdge("3", "3", "a");
+        aut2.addEdge("3", "3", "b");
+        aut2.setStart("1");
+        aut2.addFinish("3");
+
+        return aut1.intersection(aut2);
+    }
+
+    private static Automaton LoadInTest2()
+    {
+        Automaton aut1 = new Automaton();
+        Automaton aut2 = new Automaton();
+        // Test is gelukt
+
+        aut1.addEdge("1", "2", "a");
+        aut1.addEdge("2", "1", "a");
+        aut1.addEdge("1", "1", "b");
+        aut1.addEdge("2", "2", "b");
+        aut1.setStart("1");
+        aut1.addFinish("1");
+
+
+        aut2.addEdge("1", "2", "a");
+        aut2.addEdge("2", "1", "b");
+        aut2.addEdge("1", "1", "b");
+        aut2.addEdge("2", "3", "a");
+        aut2.addEdge("3", "3", "a");
+        aut2.addEdge("3", "3", "b");
+        aut2.setStart("1");
+        aut2.addFinish("1");
+
+        return aut1.intersection(aut2);
+    }
+
+
+    /** Deze automaat heeft geen eindtoestaden */
+    private static Automaton LoadInTest3()
+    {
+        Automaton aut1 = new Automaton();
+        Automaton aut2 = new Automaton();
+        // Test is gelukt
+
+        aut1.addEdge("1", "2", "a");
+        aut1.addEdge("2", "1", "a");
+        aut1.addEdge("1", "1", "b");
+        aut1.addEdge("2", "2", "b");
+        aut1.setStart("1");
+
+
+        return aut1;
+    }
+
+    /** Deze automaat heeft enkel eindtoestanden */
+    private static Automaton LoadInTest4()
+    {
+        Automaton aut1 = new Automaton();
+        Automaton aut2 = new Automaton();
+        // Test is gelukt
+
+        aut1.addEdge("1", "2", "a");
+        aut1.addEdge("2", "1", "a");
+        aut1.addEdge("1", "1", "b");
+        aut1.addEdge("2", "2", "b");
+        aut1.setStart("1");
+        aut1.addFinish("1");
+        aut1.addFinish("2");
+
+
+        return aut1;
+    }
+
+    /**
+     * Deze automaat accepteerd een aantal a's + een aantal b's
+     * Niet deterministische automaat
+     * */
+    private static Automaton LoadInTest5()
+    {
+        Automaton aut1 = new Automaton();
+        // Test is gelukt
+
+        aut1.addEdge("1", "1", "b");
+        aut1.addEdge("1", "2", "b");
+        aut1.addEdge("2", "2", "b");
+        aut1.setStart("1");
+        aut1.addFinish("2");
+
+
+        return aut1;
     }
 }
+
+
+/*
+
+ */
